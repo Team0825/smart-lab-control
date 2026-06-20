9e2c51cd22f5210afda875aed8a8fc45afeaa93d
Jeet Dey <jeetdey0825@gmail.com>
2026-06-20 22:24:40 +0530

    Add login success page test

--- monitoring/tests.py
+++ monitoring/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import LoginRecord, Session, Student
+
+
+class StudentLoginTests(TestCase):
+    def test_successful_login_renders_success_page(self):
+        student = Student.objects.create(
+            registration_number="REG001",
+            name="Test Student",
+            department="Computer Science",
+            semester=1
+        )
+        session = Session.objects.create(
+            title="Morning Lab",
+            code="123456",
+            duration=60
+        )
+
+        response = self.client.post("/", {
+            "registration": student.registration_number,
+            "session_code": session.code
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "success.html")
+        self.assertContains(response, "Welcome Test Student")
+        self.assertEqual(
+            LoginRecord.objects.filter(student=student, session=session).count(),
+            1
+        )
